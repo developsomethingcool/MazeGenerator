d9aaaddbd176a0905811f7a06569fb0de5f18ab9
developsomethingcool <pometunalexander@gmail.com>
2023-06-29 16:54:49 +0200

    Restart fixed
    Fixed bug within restart button

--- Assets/Scripts/Scripts_requiered_for_MazeGeneration/MainMenu.cs
+++ Assets/Scripts/Scripts_requiered_for_MazeGeneration/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        Debug.Log("Inside of player menu!!!!!!!!!!!!!!!!!!!");
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
