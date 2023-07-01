df2fa62c053861aa16af69a02f67a8d8f946f345
developsomethingcool <pometunalexander@gmail.com>
2023-07-01 22:07:57 +0200

    Revert "Merge branch 'hendrik' into oleksandr"
    This reverts commit 1e259196b2c7d1291fecd5fe9ad6568d6d06ed05, reversing
    changes made to 3882b93fa7c8b1e68fb33aa073a4848fc883892a.

--- Assets/Scripts/Scripts_requiered_for_Maze/GoalAreaRender.cs
+++ Assets/Scripts/Scripts_requiered_for_Maze/GoalAreaRender.cs
@@ -9,7 +9,6 @@ public class GoalAreaRender: MonoBehaviour
     //prefab of goal area
     [SerializeField] GameObject GoalArea_prefab;
     [SerializeField] MazeGenerator mazeGenerator;
-    [SerializeField] TextModifier goalText;
     [SerializeField] int killCount = 0;
     [SerializeField] int enemysTotal = 0;
 
@@ -18,6 +17,10 @@ public class GoalAreaRender: MonoBehaviour
     private GameObject dPointer;
     private bool goalUnlocked = false;
 
+    //positions of GoalArea
+    private float x;
+    private float y;
+    private float z;
 
     //
     private TextMeshProUGUI textMeshPro;
@@ -25,16 +28,13 @@ public class GoalAreaRender: MonoBehaviour
 
     void Start()
     {
-      //positions of GoalArea
-      private int x;
-      private int y;
-      private int z;
-
-        (x, z) = UniqueNumberPairGenerator.GenerateUniqueNumberPair(mazeGenerator.GetMazeHeight()/2, mazeGenerator.GetMazeHeight(), mazeGenerator.GetMazeWidth()/2, mazeGenerator.GetMazeWidth());
-
+        x = (float)Random.Range(mazeGenerator.GetMazeHeight() - 10, mazeGenerator.GetMazeHeight());
+        y = 0f;
+        z = (float)Random.Range(mazeGenerator.GetMazeHeight() - 10, mazeGenerator.GetMazeHeight());
         newCell = Instantiate(GoalArea_prefab, new Vector3(x * GoalAreaSize, y, y * GoalAreaSize), Quaternion.identity);
         textMeshPro = newCell.GetComponentInChildren<TextMeshProUGUI>();
         textMeshPro.enabled = false;
+
     }
 
     private void Update()
@@ -49,8 +49,8 @@ public class GoalAreaRender: MonoBehaviour
             }
         }
         textMeshPro.text = distanceBetweenPlayerAndPoint().ToString();
-
-
+        
+        
     }
 
 
@@ -63,7 +63,7 @@ public class GoalAreaRender: MonoBehaviour
 
     public void updateGoalArea()
     {
-        goalText.UpdateText("All Enemys Killed\nReach the goal!");
+        FindObjectOfType<TextModifier>().UpdateText("All Enemys Killed\nReach the goal!");
 
         goalUnlocked = true;
 
@@ -71,7 +71,7 @@ public class GoalAreaRender: MonoBehaviour
 
         // Set the color of the material
         goalAreaMaterial.color = new Color32(0, 255, 0, 255); // make a finish green
-
+       
         Debug.Log("Updated color of GoalArea");
         // Access the renderer component of the prefab
         Renderer prefabRenderer = newCell.GetComponent<Renderer>();
@@ -80,7 +80,7 @@ public class GoalAreaRender: MonoBehaviour
         prefabRenderer.material = goalAreaMaterial;
 
         GameObject.Find("FinishingArea").GetComponent<Renderer>().material = goalAreaMaterial;
-
+            
 
 
     }
@@ -88,13 +88,13 @@ public class GoalAreaRender: MonoBehaviour
     public void EnemyKilled()
     {
         killCount++;
-        goalText.UpdateText(killCount, enemysTotal);
+        FindObjectOfType<TextModifier>().UpdateText(killCount, enemysTotal);
     }
 
     public void UpdateGoalCondition(int eT)
     {
         enemysTotal = eT;
-        goalText.UpdateText(0, eT);
+        FindObjectOfType<TextModifier>().UpdateText(0, eT);
     }
 
     public bool goalReached()
@@ -107,7 +107,7 @@ public class GoalAreaRender: MonoBehaviour
         //float distanceSquared = Mathf.Pow((FindObjectOfType<PlayerMovment>().playersPosition().x - x),2) + Mathf.Pow((FindObjectOfType<PlayerMovment>().playersPosition().z -z),2);
         //float distance = Mathf.Sqrt(distanceSquared);
         return Vector3.Distance(FindObjectOfType<PlayerMovment>().playersPosition(), new Vector3(x,y,z));
-
+        
     }
 
 }
